c810814d9026e6384bf30d2b726ae44cad078e63
Wesley Silva <wesveio@gmail.com>
2018-04-26 02:56:44 -0300

    App - Invovando o componente Navbar e adicionando Rotas para Home e Product

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './navbar/Navbar';
+import Home from './home/Home';
+import Product from './product/Product';
 
 class App extends Component {
 	render() {
-		return <h1>Render Test</h1>;
+		return(
+			<Router>
+				<div className=''>
+					<Navbar />
+					<Route path="/" exact strict component={Home}/>
+					<Route path="/produto/:productID/:textLink" component={Product}/>
+					
+				</div>
+			</Router>
+		);
 	}
 }
 export default App;
